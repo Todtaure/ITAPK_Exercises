0a287e731a73f47adbfa7615b487951ef8dfa75a
KSBreuer <breuer.rz@gmail.com>
2015-08-31 16:09:55 +0200

    Templates, almost done.

--- Lektion_1_Templates/Lektion_1_Templates/MyArray.h
+++ Lektion_1_Templates/Lektion_1_Templates/MyArray.h
@@ -4,52 +4,75 @@ template<typename T, size_t SIZE = 10>
 class MyArray
 {
 public:
-	MyArray()
+	explicit MyArray()
 	{
-		arr_start_ = &arr[0];
-		arr_end_ = &arr[SIZE];
+		arr_start_ = &arr_[0];
+		arr_end_ = &arr_[SIZE];
+	}
+
+	template<typename D, size_t SIZE_D = 10>
+	MyArray(MyArray<D, SIZE_D> const &copy)
+	{
+		for (auto i = 0; i < copy.size(); i++)
+		{
+			arr_[i] = *(copy.begin() + i);
+		}
+
+		arr_start_ = &arr_[0];
+		arr_end_ = &arr_[SIZE];
 	}
 
 	~MyArray()
 	{
 	}
 
-	void fill(const T& obj)
+	void fill(const T &obj)
 	{
 		for (auto i = 0; i < SIZE; i++)
 		{
-			arr[i] = obj;
+			arr_[i] = obj;
 		}
 	}
 
-	T* begin()
+	T* begin() const
 	{
 		return arr_start_;
 	}
 
-	T* end()
+	T* end() const
 	{
 		return arr_end_;
 	}
 
 	T& operator[](int i)
 	{
-		return arr[i];
+		return arr_[i];
 	}
 
-	size_t size()
+	template<typename D>
+	MyArray<T, SIZE>& operator=(MyArray<D, SIZE> const &right)
+	{
+		for (int i = 0; i < SIZE;i++)
+		{
+			arr_[i] = *(right.begin() + i);
+		}
+
+		return *this;
+	}
+
+	size_t size() const
 	{
 		return SIZE;
 	}
 
 private:
-	T arr[SIZE];
+	T arr_[SIZE];
 	T* arr_start_;
 	T* arr_end_;
 };
 
-template<typename T>
-T* myFind(T* begin, T* end, const T& val)
+template<typename T, typename G>
+T* myFind(T* begin, T* end, const G& val)
 {
 	auto iter = begin;
 
@@ -65,3 +88,108 @@ T* myFind(T* begin, T* end, const T& val)
 	return end;
 }
 
+template<typename T, typename G>
+T** myFind(T** begin, T** end, const G& val)
+{
+	auto iter = begin;
+
+	while (iter != end)
+	{
+		if (**iter == val)
+		{
+			return iter;
+		}
+		++iter;
+	}
+
+	return end;
+}
+
+template<typename T, size_t SIZE>
+class MyArray<T*, SIZE>
+{
+public:
+	explicit MyArray()
+	{
+		arr_start_ = &arr_[0];
+		arr_end_ = &arr_[SIZE];
+	}
+
+	template<typename D, size_t SIZE_D = 10>
+	MyArray(MyArray<D, SIZE_D> const &copy)
+	{
+		for (auto i = 0; i < copy.size(); i++)
+		{
+			arr_[i] = *(copy.begin() + i);
+		}
+
+		arr_start_ = &arr_[0];
+		arr_end_ = &arr_[SIZE];
+	}
+
+	~MyArray()
+	{
+		for (int i = 0; i < SIZE;i++)
+		{
+			if (arr_[i] == arr_[i - 1]){ continue; }
+			delete arr_[i];
+		}
+	}
+
+	void fill(T *obj)
+	{
+		for (auto i = 0; i < SIZE; i++)
+		{
+			arr_[i] = obj;
+		}
+	}
+
+	T** begin() const
+	{
+		return arr_start_;
+	}
+
+	T** end() const
+	{
+		return arr_end_;
+	}
+
+	T* operator[](int i)
+	{
+		return (arr_[i]);
+	}
+
+	template<typename D>
+	MyArray<T, SIZE>& operator=(MyArray<D, SIZE> const &right)
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			arr_[i] = *(right.begin() + i);
+		}
+
+		return *this;
+	}
+
+	size_t size() const
+	{
+		return SIZE;
+	}
+
+private:
+	T* arr_[SIZE];
+	T** arr_start_;
+	T** arr_end_;
+};
+
+template<typename U>
+typename U::value_type myAccumulation(const U& u)
+{
+	typename U::value_type m = typename U::value_type();
+
+	for (auto first = u.begin(); first != u.end(); ++first)
+	{
+		m += *first;
+	}
+
+	return m;
+}
--- Lektion_1_Templates/Lektion_1_Templates/main.cpp
+++ Lektion_1_Templates/Lektion_1_Templates/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "MyArray.h"
 #include <boost/test/minimal.hpp>
+#include <vector>
 
 int test_main(int argc, char* argv[])
 {
@@ -16,6 +17,7 @@ int test_main(int argc, char* argv[])
 
 	arr.fill(filler);
 
+	//Exercise 1
 	for (int i = 0; i < SIZE; i++)
 	{
 		BOOST_CHECK(arr[i] == filler);
@@ -31,9 +33,52 @@ int test_main(int argc, char* argv[])
 	arr[3] = searchVal;
 	arr_ex13[3] = searchVal;
 
+	//Exercise 1.2
 	BOOST_CHECK(myFind(arr.begin(), arr.end(), searchVal) == &arr[3]);
 
-	BOOST_CHECK(myFind<double>(arr_ex13.begin(), arr_ex13.end(), searchVal) == &arr_ex13[3]);
+	//Test exercise 1.3
+	BOOST_CHECK(myFind(arr_ex13.begin(), arr_ex13.end(), searchVal) == &arr_ex13[3]);
+
+	for (int i = 0; i < SIZE;i++)
+	{
+		arr[i] = i;
+	}
+	for (int i = SIZE; i <= 0; i--)
+	{
+		arr_ex13[i] = i + 0.7;
+	}
+
+	MyArray<int, SIZE> arr_ex14(arr_ex13);
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		BOOST_CHECK(arr_ex13[i] == arr_ex14[i]);
+	}
+
+	//Test exercise 1.4
+	arr_ex13 = arr;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		BOOST_CHECK(arr_ex13[i] == arr[i]);
+	}
+
+	//Exercise 2
+	MyArray<std::string*, SIZE> arr_ex2;
+	std::string *somestring = new std::string("hello");
+
+	arr_ex2.fill(somestring);
+
+	BOOST_CHECK(*arr_ex2[0] == *somestring);
+	BOOST_CHECK(*arr_ex2.begin() == somestring);
+	BOOST_CHECK(*myFind(arr_ex2.begin(), arr_ex2.end(), "hello") == somestring);
+	
+
+	//Exercise 3
+	std::vector<int> vec = { 1,1,1,1,1,1 };
+
+	BOOST_CHECK(myAccumulation(vec) == 6);
+	BOOST_CHECK(myAccumulation(arr) == 10);
 
 	std::cout << "Test cases completed." << std::endl;
 
@@ -45,6 +90,17 @@ int test_main(int argc, char* argv[])
 Exercise 1.3
 	BOOST_CHECK(myFind<double>(arr_ex13.begin(), arr_ex13.end(), searchVal) == &arr_ex13[3]);
 	This has to be added because otherwise the compiler can´t decide whether T should be substitued by
-	int (the search parameter) or double (end and begin pointers)
+	int (the search parameter) or double (end and begin pointers)¨.
 
+Exercise 2.3
+	Dunno
+
+Exercise 3
+	1) U::value_type represents the type that the chosen container contains, eg. vector<int> it would be int.
+	   
+	2) This line calls the value_type() method which every container iterator has and it returns the type
+	of the object that is returned when the iterator is dereferenced.
+
+	3) It is requiered that the container or object used implements the iterator interface.
+	   
 */
