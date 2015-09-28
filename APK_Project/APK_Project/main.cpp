f12dd183898ce118585b209f52a3dcfd04c9ad39
CSDahl <christiansdahl@gmail.com>
2015-09-28 16:00:18 +0200

    Added tester in main and testing signals

--- APK_Project/APK_Project/main.cpp
+++ APK_Project/APK_Project/main.cpp
@@ -1,11 +1,40 @@
 #include "IntersectionStateMachine.cpp"
 #include <future>
 #include <boost/ref.hpp>
+#include <boost/bind/bind.hpp>
+#include "Log.h"
 
 int main()
 {	
 	Machine intersection;
+		
+	sigInfo.connect(Log<LogInfo>());
+	sigError.connect(Log<LogError>());
+	sigTraffic.connect(Log<LogTraffic>());
 
+	TrafficInfo test_traffic_info;
+	sigTraffic.connect(boost::bind(&TrafficInfo::TrafficMilestone, boost::ref(test_traffic_info), _1));
+
+
+	LogInfo testInfo;
+	testInfo.message = "Green light";
+	sigInfo(testInfo);
+
+	LogError testError;
+	testError.message = "Crash and burn";
+	sigError(testError);
+
+	LogTraffic testTraffic;
+	testTraffic.direction = LogTraffic::NORTH;
+	testTraffic.numberOfEmergencies = 1;
+	testTraffic.numberOfVehicles = 142;
+	sigTraffic(testTraffic);
+
+	testTraffic.numberOfVehicles = 242;
+	sigTraffic(testTraffic);
+
+	
+	
 	getchar();
 
 	return 0;
