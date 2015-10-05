6782de8dc93e59fa489d373b0772415f803e73c2
Todtaure <razak.todtaure@gmail.com>
2015-10-05 13:57:41 +0200

    Statemachine working.

--- APK_Project/APK_Project/IntersectionStateMachine.cpp
+++ APK_Project/APK_Project/IntersectionStateMachine.cpp
@@ -6,8 +6,12 @@
 #include <boost/statechart/custom_reaction.hpp>
 #include <iostream>
 #include <thread>
+#include "SCUtils.h"
+#include <vector>
 
 namespace sc = boost::statechart;
+using namespace SCUtils;
+
 
 /*
 State translations:
@@ -20,20 +24,9 @@ LightState_5	NS-Red, EW-Yellow
 LightState_6	NS-Red, EW-RedYellow
 
 */
+#define TIMER std::chrono::seconds(1)
+#define EM_TIMER std::chrono::seconds(4)
 
-enum LightStates
-{
-	NORMALEXECUTION,
-	INITIALRR,
-	LS_1,
-	LS_2,
-	LS_3,
-	LS_4,
-	LS_5,
-	LS_6
-};
-
-int const wait_ = 1;
 
 /*
 Events
@@ -46,6 +39,34 @@ struct EvToLS5 : sc::event<EvToLS5> {};
 struct EvToLS6 : sc::event<EvToLS6> {};
 struct EvToRR : sc::event<EvToRR> {};
 
+struct EvEmS1 : sc::event<EvEmS1> {};
+
+struct EvEmS2 : sc::event<EvEmS2>
+{
+	EvEmS2(Direction ev_direction) : navigate_to_(ev_direction)
+	{}
+
+	Direction navigate_to_;
+};
+
+struct EvEmS3 : sc::event<EvEmS3>
+{
+	EvEmS3(Direction ev_direction) : navigate_to_(ev_direction)
+	{}
+
+	Direction navigate_to_;
+};
+
+struct EvEmS4 : sc::event<EvEmS4> {};
+
+struct EvEmRR : sc::event<EvEmRR>
+{
+	EvEmRR(Direction ev_direction) : navigate_to_(ev_direction)
+	{}
+
+	Direction navigate_to_;
+};
+
 struct EvEVApproaching : sc::event<EvEVApproaching> {};
 struct EvEVPassed : sc::event<EvEVPassed> {};
 
@@ -56,9 +77,11 @@ StateMachine
 
 struct Operational;
 struct YellowFlashing;
+struct Emergency;
 
 struct Machine : sc::state_machine<Machine, Operational>
 {
+
 	//Entry
 	Machine()
 	{
@@ -71,8 +94,16 @@ struct Machine : sc::state_machine<Machine, Operational>
 
 	}
 
+	void set_emergency_interrupt(Direction direction)
+	{
+		emergency_interrupt_ = true;
+		ev_direction_ = direction;
+	}
+
 	bool emergency_interrupt_ = false;
 	bool error_interupt_ = false;
+
+	Direction ev_direction_;
 };
 
 
@@ -86,7 +117,7 @@ struct Emergency;
 struct Operational : sc::simple_state<Operational, Machine, NormalExecution>
 {
 	//Entry
-	Operational()
+	Operational() : previous_state_(LightStates::NORMALEXECUTION)
 	{
 
 	}
@@ -96,6 +127,8 @@ struct Operational : sc::simple_state<Operational, Machine, NormalExecution>
 	{
 
 	}
+
+	LightStates previous_state_;
 };
 
 struct YellowFlashing : sc::simple_state<YellowFlashing, Machine>
@@ -118,14 +151,19 @@ struct YellowFlashing : sc::simple_state<YellowFlashing, Machine>
 Tier-II states
 */
 struct InitialRR;
-struct InitialRR_Emergency;
+
+struct EM_LightState_1;
+struct EM_LightState_2;
+struct EM_LightState_3;
+struct EM_LightState_4;
+struct EM_InitialRR;
+struct EM_Choice;
 
 struct NormalExecution : sc::simple_state<NormalExecution, Operational, InitialRR>
 {
 	//Entry
 	NormalExecution()
 	{
-		previous_state_ = LightStates::NORMALEXECUTION;
 	}
 
 	//Exit
@@ -134,24 +172,22 @@ struct NormalExecution : sc::simple_state<NormalExecution, Operational, InitialR
 
 	}
 
-	LightStates previous_state_;
-
 	//Events
-	typedef sc::transition<EvEVApproaching, Emergency> reactions;
 };
 
-struct Emergency : sc::simple_state<Emergency, Operational>
+struct Emergency : sc::state<Emergency, Operational, EM_Choice>
 {
 	//Entry
-	Emergency()
+	Emergency(my_context ctx) : my_base(ctx)
 	{
-		std::cout << "***" << std::endl << "Entering emergency state!" << std::endl << "***" << std::endl;
+		std::cout << "***" << std::endl << "Entering emergency state!" << std::endl << "***" << std::endl;		
 	}
 
 	//Exit
 	~Emergency()
 	{
-
+		std::cout << "***" << std::endl << "Leaving emergency state!" << std::endl << "***" << std::endl;
+		context<Machine>().emergency_interrupt_ = false;
 	}
 };
 
@@ -165,172 +201,415 @@ struct LightState_4;
 struct LightState_5;
 struct LightState_6;
 
+
 /*
 * NorthSouth: Red
 * EastWest: Red
 */
-struct InitialRR : sc::simple_state<InitialRR, NormalExecution>
+struct InitialRR : sc::state<InitialRR, NormalExecution>
 {
 	//Entry
-	InitialRR()
+	InitialRR(my_context ctx) : my_base(ctx)
 	{
+		//Check for emergency vehicle approaching.
+		if (context<Machine>().emergency_interrupt_) { post_event(EvEVApproaching()); return; }
+
 		std::cout << "***" << std::endl;
 		std::cout << "Initial state." << std::endl << "North-South: Red" << std::endl << "East-West: Red" << std::endl;
 
-		std::this_thread::sleep_for(std::chrono::seconds(wait_));
+		std::this_thread::sleep_for(TIMER);
+
+		//Nort-South have had green, thus its East-West's turn
+		if (context<Operational>().previous_state_ == LS_2 || context<Operational>().previous_state_ == NORMALEXECUTION)
+		{
+			std::cout << " *EVENT* " << "PostEvent ToSL6" << std::endl;
+			post_event(EvToLS6());
+		}
+		else
+		{
+			std::cout << " *EVENT* " << "PostEvent ToSL3" << std::endl;
+			post_event(EvToLS3());
+		}
 	}
 
 	//Exit
 	~InitialRR()
 	{
-		context<NormalExecution>().previous_state_ = INITIALRR;
+		context<Operational>().previous_state_ = INITIALRR;
 	}
 
 	//Events
-	typedef boost::mpl::list<sc::transition<EvToLS6, LightState_6>, sc::transition<EvToLS3, LightState_3> > reactions;
+	typedef boost::mpl::list<sc::transition<EvToLS6, LightState_6>, sc::transition<EvToLS3, LightState_3>, sc::transition<EvEVApproaching, Emergency> > reactions;
 };
 
 /*
 * NorthSouth: Green
 * EastWest: Red
 */
-struct LightState_1 : sc::simple_state<LightState_1, NormalExecution>
+struct LightState_1 : sc::state<LightState_1, NormalExecution>
 {
 	//Entry
-	LightState_1()
+	LightState_1(my_context ctx) : my_base(ctx)
 	{
+		//Check for emergency vehicle approaching.
+		if (context<Machine>().emergency_interrupt_) { post_event(EvEVApproaching()); return; }
+
 		std::cout << "***" << std::endl << "LightState _1." << std::endl << "North-South: Green" << std::endl << "East-West: Red" << std::endl;
 
-		std::this_thread::sleep_for(std::chrono::seconds(wait_));
+		std::this_thread::sleep_for(TIMER);
+
+		std::cout << " *EVENT* " << "PostEvent ToSL2" << std::endl;
+		post_event(EvToLS2());
 	}
 
 	//Exit
 	~LightState_1()
 	{
-		context<NormalExecution>().previous_state_ = LS_1;
+		context<Operational>().previous_state_ = LS_1;
 	}
 
 	//Events
-	typedef sc::transition<EvToLS2, LightState_2> reactions;
+	typedef boost::mpl::list<sc::transition<EvToLS2, LightState_2>, sc::transition<EvEVApproaching, Emergency> > reactions;
 };
 
 /*
 * NorthSouth: Yellow
 * EastWest: Red
 */
-struct LightState_2 : sc::simple_state<LightState_2, NormalExecution>
+struct LightState_2 : sc::state<LightState_2, NormalExecution>
 {
 	//Entry
-	LightState_2()
+	LightState_2(my_context ctx) : my_base(ctx)
 	{
+		//Check for emergency vehicle approaching.
+		if (context<Machine>().emergency_interrupt_) { post_event(EvEVApproaching()); return; }
+
 		std::cout << "***" << std::endl << "LightState_2" << std::endl << "North-South: Yellow" << std::endl << "East-West: Red" << std::endl;
-		
-		std::this_thread::sleep_for(std::chrono::seconds(wait_));
+
+		std::this_thread::sleep_for(TIMER);
+
+		std::cout << " *EVENT* " << "PostEvent EvToRR" << std::endl;
+		post_event(EvToRR());
 	}
 
 	//Exit
 	~LightState_2()
 	{
-		context<NormalExecution>().previous_state_ = LS_2;
+		context<Operational>().previous_state_ = LS_2;
 	}
 
 	//Events
-	typedef sc::transition<EvToRR, InitialRR> reactions;
+	typedef boost::mpl::list<sc::transition<EvToRR, InitialRR>, sc::transition<EvEVApproaching, Emergency> > reactions;
 };
 
 /*
 * NorthSouth: RedYellow
 * EastWest: Red
 */
-struct LightState_3 : sc::simple_state<LightState_3, NormalExecution>
+struct LightState_3 : sc::state<LightState_3, NormalExecution>
 {
 	//Entry
-	LightState_3()
+	LightState_3(my_context ctx) : my_base(ctx)
 	{
+		//Check for emergency vehicle approaching.
+		if (context<Machine>().emergency_interrupt_) { post_event(EvEVApproaching()); return; }
+
 		std::cout << "***" << std::endl << "LightState_3" << std::endl << "North-South: RedYellow" << std::endl << "East-West: Red" << std::endl;
-		
-		std::this_thread::sleep_for(std::chrono::seconds(wait_));
+
+		std::this_thread::sleep_for(TIMER);
+
+		std::cout << " *EVENT* " << "PostEvent ToSL1" << std::endl;
+		post_event(EvToLS1());
 	}
 
 	//Exit
 	~LightState_3()
 	{
-		context<NormalExecution>().previous_state_ = LS_3;
+		context<Operational>().previous_state_ = LS_3;
 	}
 
 	//Events
-	typedef sc::transition<EvToLS1, LightState_1> reactions;
+	typedef boost::mpl::list<sc::transition<EvToLS1, LightState_1>, sc::transition<EvEVApproaching, Emergency> > reactions;
 };
 
 /*
 * NorthSouth: Red
 * EastWest: Green
 */
-struct LightState_4 : sc::simple_state<LightState_4, NormalExecution>
+struct LightState_4 : sc::state<LightState_4, NormalExecution>
 {
 	//Entry
-	LightState_4()
+	LightState_4(my_context ctx) : my_base(ctx)
 	{
+		//Check for emergency vehicle approaching.
+		if (context<Machine>().emergency_interrupt_) { post_event(EvEVApproaching()); return; }
+
 		std::cout << "***" << std::endl << "LightState_4" << std::endl << "North-South: Red" << std::endl << "East-West: Green" << std::endl;
-		
-		std::this_thread::sleep_for(std::chrono::seconds(wait_));
+
+		std::this_thread::sleep_for(TIMER);
+
+		std::cout << " *EVENT* " << "PostEvent ToSL5" << std::endl;
+		post_event(EvToLS5());
 	}
 
 	//Exit
 	~LightState_4()
 	{
-		context<NormalExecution>().previous_state_ = LS_4;
+		context<Operational>().previous_state_ = LS_4;
 	}
 
 	//Events
-	typedef sc::transition<EvToLS5, LightState_5> reactions;
+	typedef boost::mpl::list<sc::transition<EvToLS5, LightState_5>, sc::transition<EvEVApproaching, Emergency> >  reactions;
 };
 
 /*
 * NorthSouth: Red
 * EastWest: Yellow
 */
-struct LightState_5 : sc::simple_state<LightState_5, NormalExecution>
+struct LightState_5 : sc::state<LightState_5, NormalExecution>
 {
 	//Entry
-	LightState_5()
+	LightState_5(my_context ctx) : my_base(ctx)
 	{
+		//Check for emergency vehicle approaching.
+		if (context<Machine>().emergency_interrupt_) { post_event(EvEVApproaching()); return; }
+
 		std::cout << "***" << std::endl << "LightState_5" << std::endl << "North-South: Red" << std::endl << "East-West: Yellow" << std::endl;
-		
-		std::this_thread::sleep_for(std::chrono::seconds(wait_));
+
+		std::this_thread::sleep_for(TIMER);
+
+		std::cout << " *EVENT* " << "PostEvent EvToRR" << std::endl;
+		post_event(EvToRR());
 	}
 
 	//Exit
 	~LightState_5()
 	{
-		context<NormalExecution>().previous_state_ = LS_5;
+		context<Operational>().previous_state_ = LS_5;
 	}
 
 	//Events
-	typedef sc::transition<EvToRR, InitialRR> reactions;
+	typedef boost::mpl::list<sc::transition<EvToRR, InitialRR>, sc::transition<EvEVApproaching, Emergency> > reactions;
 };
 
 /*
 * NorthSouth: Red
 * EastWest: RedYellow
 */
-struct LightState_6 : sc::simple_state<LightState_6, NormalExecution>
+struct LightState_6 : sc::state<LightState_6, NormalExecution>
 {
 	//Entry
-	LightState_6()
+	LightState_6(my_context ctx) : my_base(ctx)
 	{
+		//Check for emergency vehicle approaching.
+		if (context<Machine>().emergency_interrupt_) { post_event(EvEVApproaching()); return; }
+
 		std::cout << "***" << std::endl << "LightState_6" << std::endl << "North-South: Red" << std::endl << "East-West: RedYellow" << std::endl;
 
-		std::this_thread::sleep_for(std::chrono::seconds(wait_)); 
+		std::this_thread::sleep_for(TIMER);
+
+		std::cout << " *EVENT* " << "PostEvent ToSL4" << std::endl;
+		post_event(EvToLS4());
 	}
 
 	//Exit
 	~LightState_6()
 	{
-		context<NormalExecution>().previous_state_ = LS_6;
+		context<Operational>().previous_state_ = LS_6;
 	}
 
 	//Events
-	typedef sc::transition<EvToLS4, LightState_4> reactions;
+	typedef boost::mpl::list<sc::transition<EvToLS4, LightState_4>, sc::transition<EvEVApproaching, Emergency> > reactions;
 };
 
+
+/*
+Tier-III Emergency states
+*/
+
+struct EM_LightState_1;
+struct EM_LightState_2;
+struct EM_LightState_3;
+struct EM_LightState_4;
+struct EM_InitialRR;
+
+
+struct EM_LightState_1 : sc::state<EM_LightState_1, Emergency>
+{
+	//Entry
+	EM_LightState_1(my_context ctx) : my_base(ctx)
+	{
+		std::cout << "***" << std::endl << "Emergency LightState_1" << std::endl << "North-South: Green" << std::endl << "East-West: Red" << std::endl;
+		
+		std::this_thread::sleep_for(EM_TIMER);
+		post_event(EvEVPassed());
+	}
+
+	//Exit
+	~EM_LightState_1()
+	{
+		context<Operational>().previous_state_ = LightStates::LS_1;
+	}
+
+	//Events
+	typedef sc::transition<EvEVPassed, NormalExecution> reactions;
+
+};
+
+struct EM_LightState_2 : sc::state<EM_LightState_2, Emergency>
+{
+	//Entry
+	EM_LightState_2(my_context ctx) : my_base(ctx)
+	{
+		std::cout << "***" << std::endl << "Emergency LightState_2" << std::endl << "North-South: Red" << std::endl << "East-West: Yellow" << std::endl;
+	}
+
+	//Exit
+	~EM_LightState_2()
+	{
+	}
+
+	//Events
+	typedef sc::custom_reaction<EvEmS2> reactions;
+
+	sc::result react(const EvEmS2& e)
+	{
+		post_event(EvEmRR(e.navigate_to_));
+		return transit<EM_InitialRR>();
+	}
+};
+
+struct EM_LightState_3 : sc::state<EM_LightState_3, Emergency>
+{
+	//Entry
+	EM_LightState_3(my_context ctx) : my_base(ctx)
+	{
+		std::cout << "***" << std::endl << "Emergency LightState_3" << std::endl << "North-South: Yellow" << std::endl << "East-West: Red" << std::endl;
+	}
+
+	//Exit
+	~EM_LightState_3()
+	{
+	}
+
+	//Events
+	typedef sc::custom_reaction<EvEmS3> reactions;
+
+
+	sc::result react(const EvEmS3& e)
+	{
+		post_event(EvEmRR(e.navigate_to_));
+		return transit<EM_InitialRR>();
+	}
+};
+
+struct EM_LightState_4 : sc::state<EM_LightState_4, Emergency>
+{
+	//Entry
+	EM_LightState_4(my_context ctx) : my_base(ctx)
+	{
+		std::cout << "***" << std::endl << "Emergency LightState_4" << std::endl << "North-South: Red" << std::endl << "East-West: Green" << std::endl;
+		
+		std::this_thread::sleep_for(EM_TIMER);
+		post_event(EvEVPassed());
+	}
+
+	//Exit
+	~EM_LightState_4()
+	{
+		context<Operational>().previous_state_ = LightStates::LS_4;
+	}
+
+	//Events
+	typedef sc::transition<EvEVPassed, NormalExecution> reactions;
+};
+
+struct EM_InitialRR : sc::state<EM_InitialRR, Emergency>
+{
+	//Entry
+	EM_InitialRR(my_context ctx) : my_base(ctx)
+	{
+		std::cout << "***" << std::endl << "Emergency initialRR" << std::endl << "North-South: Red" << std::endl << "East-West: Red" << std::endl;
+	}
+
+	//Exit
+	~EM_InitialRR()
+	{
+	}
+
+	//Events
+	typedef boost::mpl::list<sc::custom_reaction<EvEmRR>, sc::transition<EvEmS1, EM_LightState_1>, sc::transition<EvEmS4, EM_LightState_4> > reactions;
+
+	sc::result react(const EvEmRR& e)
+	{
+		if (e.navigate_to_ == North || e.navigate_to_ == South)
+		{
+			post_event(EvEmS1());
+		}
+		else
+		{
+			post_event(EvEmS4());
+		}
+
+		return discard_event();
+	}
+};
+
+struct EM_Choice : sc::state<EM_Choice, Emergency>
+{
+	//Entry
+	EM_Choice(my_context ctx) : my_base(ctx)
+	{
+		std::cout << "***" << std::endl << "Current LightState is: " << context<Operational>().previous_state_ << std::endl;
+		std::cout << "***" << std::endl << "Emergency Vehicle going: " << context<Machine>().ev_direction_ << std::endl;
+
+		LightStates current = context<Operational>().previous_state_;
+		Direction current_direction = context<Machine>().ev_direction_;
+
+		switch (current)
+		{
+		case LightStates::LS_1:
+		{
+			post_event(EvEmS2(current_direction));
+			break;
+		}
+		case LightStates::LS_4:
+		{
+			post_event(EvEmS3(current_direction));
+			break;
+		}
+		default:
+		{
+			post_event(EvEmRR(current_direction));
+			break;
+		}
+		}
+	}
+
+	//Exit
+	~EM_Choice()
+	{
+		
+	}
+
+	//Events
+	typedef boost::mpl::list<sc::custom_reaction<EvEmS2>, sc::custom_reaction<EvEmS3>, sc::custom_reaction<EvEmRR> > reactions;
+
+	sc::result react(const EvEmS2& e)
+	{
+		post_event(e);
+		return transit<EM_LightState_2>();
+	}
+
+	sc::result react(const EvEmS3& e)
+	{
+		post_event(e);
+		return transit<EM_LightState_3>();
+	}
+
+	sc::result react(const EvEmRR& e)
+	{
+		post_event(e);
+		return transit<EM_InitialRR>();
+	}
+};
